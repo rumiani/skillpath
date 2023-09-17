e1cf56d42fa597694e3fb2830228f8580ec1398d
rumiani <maziar.rumiani@gmail.com>
2023-09-17 20:07:46 +0330

    fixing updated component message

--- components/hookFormComps/cancelBtn/cancelBtn.tsx
+++ components/hookFormComps/cancelBtn/cancelBtn.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import { useSelector } from "react-redux";
 
 const CancelBtn = () => {
-  const router = useRouter()  
+  const router = useRouter();
   return (
-      <Link href={"/path/" + router.query.url}>
-    <button className="cancelBtn">
-        CANCEL
-    </button>
-      </Link>
+    <Link href={"/path/" + router.query.url}>
+      <button className="cancelBtn">CANCEL</button>
+    </Link>
   );
 };
 
